62dce228ca3b6ff3aaa2c44f672f1d23b028320a
BaranovIgor <60816863+BaranovIgor@users.noreply.github.com>
2020-02-08 18:38:27 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
 
 TOKEN='1016794449:AAG7PN8Gjf47cTAhpKFR7Isb1fFSATfaaho'
-REQUEST_KWARGS={
-    'proxy_url': 'socks5://s5.priv.opennetwork.cc:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'v3_470788620',
-        'password': 'BJBANUXu',
-    }
-}
 
-updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
+updater = Updater(token=TOKEN)  # тут токен, который выдал вам Ботский Отец!
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует
                                                 # только на команду /start
